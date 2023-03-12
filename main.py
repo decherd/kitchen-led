fba8595f1e481d778a549c74a926c75c59a3da83
Drew Echerd <drewecherd@gmail.com>
2023-03-12 15:10:39 +0500

    fixed ssid and password missing

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def connect():
     #Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.connect(ssid, password)
+    wlan.connect('TTLG', '31012023g')
     while wlan.isconnected() == False:
         print('Waiting for connection...')
         sleep(1)
