16be72000427ce4e33bb70187468a77e854d4476
Drew Echerd <drewecherd@gmail.com>
2023-03-12 14:41:48 +0500

    added log file and socket server

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 import ntptime
 from machine import Pin, PWM
 import network
+from os.path import exists
+
 
 UTC_TIMEZONE = 5
 LEDS_PIN = 32
@@ -10,6 +12,27 @@ PIR_PIN = 33
 ntptime.settime()
 utc_offset = UTC_TIMEZONE * 60 * 60
 actual_time = time.localtime(time.time() + utc_offset)
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    return ip
+
+def open_socket(ip):
+    # Open a socket
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    return connection
+
 
 frequency = 5000
 led = PWM(Pin(LEDS_PIN), frequency)
@@ -54,7 +77,16 @@ SLEEP_MODE_ALARM_MINUTES = 5
 pir = Pin(PIR_PIN, Pin.IN)
 pir.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
 sleep_start = 0
+connection = open_socket(ip)
+
 while True:
+    client = connection.accept()[0]
+    request = client.recv(1024)
+    request = str(request)
+    if exists('log.txt'):
+        with open('log.txt', 'r') as f:
+            client.send(f)
+    client.close()
     actual_time = time.localtime(time.time() + utc_offset)
     # If it is Sleep Mode
     if (actual_time[3], actual_time[4]) >= SLEEP_MODE_START or (actual_time[3], actual_time[4]) < DAWN_START:
@@ -62,9 +94,13 @@ while True:
             led_ramp_to(SLEEP_MODE_PCT)
             sleep_start = time.time()
             motion = False
+            with open('log.txt' mode='a+') as f:
+                f.write(f"Detecting motion at: {actual_time}")
         if sleep_start and (time.time() - sleep_start >= (SLEEP_MODE_ALARM_MINUTES*60)):
             led_ramp_to(0)
             sleep_start = 0
+            with open('log.txt' mode='a+') as f:
+                f.write(f"Turning off LED at: {actual_time}")
     # Dawn/Dusk Mode turn up/down if motion for first time or if it was already on
     elif ((actual_time[3], actual_time[4]) >= DAWN_START and (actual_time[3], actual_time[4]) < DAY_START) or (((actual_time[3], actual_time[4]) >= DUSK_START and (actual_time[3], actual_time[4]) < SLEEP_MODE_START)):
         if motion or led.duty() > 0:
